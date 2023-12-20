2e0cbdb897735c3bf3caae9efc56edccace3a9fd
Gradeupai <148335339+Gradeupai@users.noreply.github.com>
2023-12-20 23:47:01 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,6 @@
 sudo yum update
 sudo yum install -y python3 python3-pip
 pip3 install virtualenv
-pip install gunicorn
 
 # Create our working directory if it doesn't exist
 DIR="/home/ec2-user/Flask-App"
