c786c97691ca87f5b12c9ed26ac197540c13b602
Gradeupai <148335339+Gradeupai@users.noreply.github.com>
2023-12-20 20:28:45 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install node
+# Install Python and virtualenv
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
+pip3 install virtualenv
 
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
+# Create our working directory if it doesn't exist
+DIR="/home/ec2-user/Flask-App"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
 else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
+
+# Create and activate virtual environment
+cd /home/ec2-user/Flask-App
+python3 -m virtualenv venv
+source venv/bin/activate
