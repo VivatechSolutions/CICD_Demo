8dbe95b25d76235d27ab0547a900662da8ccbd97
Gradeupai <148335339+Gradeupai@users.noreply.github.com>
2023-12-20 22:42:51 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Stop Gunicorn
-echo "Stopping Gunicorn server..."
-sudo pkill -f gunicorn
-
-# Wait for Gunicorn to stop (adjust the sleep duration as needed)
-sleep 5
-
-# Verify that Gunicorn has stopped
-if pgrep -f gunicorn > /dev/null
-then
-  echo "Failed to stop Gunicorn."
-  exit 1
-else
-  echo "Gunicorn stopped successfully."
-fi
-
-exit 0
+# Stopping existing Flask servers
+echo "Stopping any existing Flask servers"
+pkill gunicorn
