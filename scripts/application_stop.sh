12a864a470989076fcaf1a7bf150cdf6610fd98a
Gradeupai <148335339+Gradeupai@users.noreply.github.com>
2023-12-21 21:10:02 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,21 @@
-#!/bin/bash
+
 
 # Stopping existing Flask servers
+#echo "Stopping any existing Flask servers"
+#pkill gunicorn
+
+#!/bin/bash
+
+# Stop the Flask application
+
 echo "Stopping any existing Flask servers"
-pkill gunicorn
+
+# Find the Flask server process and kill it
+pkill -f "python -m flask run"
+
+# Optionally, you may want to add a delay before checking if the process is still running
+sleep 2
+
+# Check if the process is still running and force kill if necessary
+pkill -9 -f "python -m flask run"
+
