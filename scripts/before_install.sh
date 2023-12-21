c854db77704cc40f571a3bc20b0612f54679351a
Gradeupai <148335339+Gradeupai@users.noreply.github.com>
2023-12-21 21:13:07 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,12 +1,37 @@
+# #!/bin/bash
+
+# # Install Python and virtualenv
+# sudo yum update
+# sudo yum install -y python3 python3-pip
+# pip3 install virtualenv
+
+# # Install Gunicorn
+# pip3 install gunicorn
+
+# # Create our working directory if it doesn't exist
+# DIR="/home/ec2-user/Flask-App"
+# if [ -d "$DIR" ]; then
+#   echo "${DIR} exists"
+# else
+#   echo "Creating ${DIR} directory"
+#   mkdir ${DIR}
+# fi
+
+# # Create and activate virtual environment
+# cd /home/ec2-user/Flask-App
+# python3 -m virtualenv venv
+# source venv/bin/activate
+
+
+##### New one 
+
+
 #!/bin/bash
 
 # Install Python and virtualenv
 sudo yum update
 sudo yum install -y python3 python3-pip
 pip3 install virtualenv
-
-# Install Gunicorn
-pip3 install gunicorn
 
 # Create our working directory if it doesn't exist
 DIR="/home/ec2-user/Flask-App"
@@ -21,3 +46,4 @@ fi
 cd /home/ec2-user/Flask-App
 python3 -m virtualenv venv
 source venv/bin/activate
+
