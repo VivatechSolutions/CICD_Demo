d99ceaa81c1c447246d5a5400e543cc685343702
Gradeupai <148335339+Gradeupai@users.noreply.github.com>
2024-02-21 02:31:56 +0530

    commit

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-load_dotenv()
 from langchain.vectorstores import Qdrant
 from langchain.embeddings.openai import OpenAIEmbeddings
 import qdrant_client
@@ -8,45 +7,24 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
-from pymongo import MongoClient
 from datetime import datetime
 import os
 import uuid
-from langchain.prompts.chat import SystemMessagePromptTemplate
-
 
 app = Flask(__name__)
-app.secret_key = "your_secret_key"  # Change this to a secure random value in production
 
-# MongoDB URI
-# mongodb_uri = "mongodb+srv://darshan:VG4inwNtIDZEvLrF@cluster1.4b09mmq.mongodb.net/?retryWrites=true&w=majority"
+secret_key = os.urandom(24)
+app.secret_key = secret_key.hex()
 
-# OPENAI_API_KEY= os.getenv('OPENAI_API_KEY')
+# app.secret_key = "your_secret_key"  # Change this to a secure random value in production
 
-
+# Qdrant configuration
 QDRANT_HOST = os.getenv('QDRANT_HOST')
 app.config['QDRANT_HOST'] = QDRANT_HOST
 QDRANT_API_KEY = os.getenv('QDRANT_API_KEY')
 app.config['QDRANT_API_KEY'] = QDRANT_API_KEY
 QDRANT_COLLECTION = os.getenv('QDRANT_COLLECTION')
 app.config['QDRANT_COLLECTION'] = QDRANT_COLLECTION
-mongodb_uri = os.getenv('mongodb_uri')
-app.config['mongodb_uri'] = QDRANT_HOST
-
-# Connect to MongoDB
-client = MongoClient(mongodb_uri)
-mongo_db = client['viva']
-mongo_collection = mongo_db['chat_history']
-
-def send_message(user, message):
-    timestamp = datetime.now()
-    data = {'user': user, 'message': message, 'timestamp': timestamp}
-    mongo_collection.insert_one(data)
-
-def get_chat_history(user):
-    mongo_history = mongo_collection.find({'user': "f8786ba3-802e-4a13-bad3-ffc2cfa1cb69"})
-    return [message['message'] for message in mongo_history]
-
 
 def get_vector_store():
     client = qdrant_client.QdrantClient(QDRANT_HOST, api_key=QDRANT_API_KEY)
@@ -70,17 +48,8 @@ template = """
 
 prompt = PromptTemplate(input_variables=["chat_history", "context", "question"], template=template)
 
-from openai.error import OpenAIError
-
-def get_conversation_chain(vector_store, prompt):
+def get_conversation_chain(vector_store, prompt, chat_history):
     memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')
-    print(memory)
-    user_id = get_or_create_user_id()
-    chat_history = get_chat_history(user_id)
-    print(chat_history)
-    # print("Is chat_history a list of strings?", all(isinstance(msg, str) for msg in chat_history))
-    formatted_chat_history = [{'message': msg} for msg in chat_history]
-    print("Formatted chat history:", formatted_chat_history)
 
     try:
         qa = ConversationalRetrievalChain.from_llm(
@@ -89,37 +58,41 @@ def get_conversation_chain(vector_store, prompt):
             return_source_documents=True,
             verbose=True,
             chain_type="stuff",
-            get_chat_history=lambda h: formatted_chat_history,
+            get_chat_history=lambda h: chat_history,
             combine_docs_chain_kwargs={'prompt': prompt},
             memory=memory
         )
         return qa
-    except OpenAIError as e:
-        # Handle rate limit error
-        print("OpenAI API Rate Limit Exceeded. Please wait and retry later.")
+    except Exception as e:
+        print(f"Error: {e}")
         raise
-
 
 @app.route('/ask', methods=['POST'])
 def ask_question():
-    user_question = request.json['question']
-    user_id = get_or_create_user_id()
-    conversation_chain1 = get_conversation_chain(get_vector_store(), prompt)
-    response = handle_userinput(user_question, conversation_chain1, user_id)
+    request_data = request.json
+    user_question = request_data['question']
+    chat_history = request_data['chat_history']
+
+    conversation_chain1 = get_conversation_chain(get_vector_store(), prompt, chat_history)
+    response = handle_user_input(user_question, conversation_chain1, chat_history)
+    
     return jsonify({'response': response['answer']})
 
-def get_or_create_user_id():
-    if 'user_id' not in session:
-        session['user_id'] = str(uuid.uuid4())
-    return session['user_id']
+@app.route('/v1/ask', methods=['POST'])
+def ask_question_v1():
+    request_data = request.json
+    user_question = request_data['question']
+    chat_history = request_data['chat_history']
 
-def handle_userinput(user_question, conversation_chain1, user_id):
-    chat_history = get_chat_history(user_id)
+    conversation_chain1 = get_conversation_chain(get_vector_store(), prompt, chat_history)
+    response = handle_user_input(user_question, conversation_chain1, chat_history)
+    
+    return jsonify({'response': response['answer']})
+
+
+def handle_user_input(user_question, conversation_chain1, chat_history):
     response = conversation_chain1({'question': user_question, 'chat_history': chat_history})
-    send_message(user_id, user_question)
-    send_message(user_id, response['answer'])
     return {"answer": response['answer']}
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
